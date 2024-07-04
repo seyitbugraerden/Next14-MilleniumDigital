19d17d4cf8fe76d35cf0f3233ea7d1c98f536cd6
seyitbugraerden <seyitbugraerden@gmail.com>
2024-07-04 23:25:57 +0300

    images section with fliptext

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,15 @@
+import { ImagesSliderDemo } from "@/components/ImagesSliderDemo";
+import { FlipWords } from "@/components/ui/flip-words";
+
 export default function Home() {
+  
+
   return (
     <>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit,
-      id voluptates magni inventore repellendus praesentium expedita incidunt
-      itaque repellat aut excepturi error voluptatum autem illum vitae deleniti
-      distinctio? Facere porro ducimus qui deleniti est eligendi, tempore odio
-      impedit distinctio nobis vero reiciendis eius, numquam placeat explicabo,
-      voluptas doloribus ipsa excepturi quos quo unde nihil aspernatur maiores.
-      Quam doloribus deleniti quod dolore, dignissimos molestiae aspernatur!
-      Eveniet, reprehenderit! Reiciendis quod libero pariatur sed tempora, quae
-      sit eum quia suscipit ipsam quos ullam veritatis ipsum molestias dolorem
-      soluta iure similique officiis voluptate veniam amet autem! Blanditiis,
-      alias beatae? Quis repellendus harum cumque omnis voluptatum mollitia
-      dicta totam pariatur maiores laborum, excepturi amet id illum beatae
-      velit, quasi temporibus, voluptas magnam magni unde veritatis aspernatur
-      delectus. Similique quaerat culpa necessitatibus quam! Distinctio maiores
-      exercitationem error doloribus quaerat hic aliquid, facere iste id,
-      blanditiis totam ex corporis maxime neque excepturi illum consequuntur
-      libero quisquam perferendis iusto sint quidem ut itaque. Dolores animi
-      voluptatum vitae sunt iure repudiandae quibusdam. Doloribus inventore
-      rerum ipsam explicabo delectus tempore ratione, quidem facilis itaque
-      maxime voluptate odio velit hic? Vitae aperiam veritatis aliquam numquam,
-      repellat totam reiciendis quas, quasi quod amet ea cumque, eveniet esse
-      incidunt dicta porro distinctio corporis!
+      <ImagesSliderDemo />
+      <h1>
+        Dilediğiniz 
+      </h1>
     </>
   );
 }
