a863936243ac7242a01716d022c715e98f98d65b
seyitbugraerden <seyitbugraerden@gmail.com>
2024-08-25 22:51:42 +0300

    fixes

--- app/hizmetlerimiz/page.tsx
+++ app/hizmetlerimiz/page.tsx
@@ -5,65 +5,119 @@ function Hizmetlerimiz() {
   return (
     <div className="hizmetler">
       <div className="grid md:grid-cols-2 2xl:grid-cols-3 m-0 p-0">
-        <Link href="/hizmetlerimiz/web-site-cozumleri" className="card-area relative overflow-hidden">
-          <div className="text-area">
-            <p>01</p>
-            <h2>Web Site Çözümleri</h2>
-            <p>
-            Millenium Digital, web sitesi çözümleri sunan bir firmadır. Kullanıcı dostu, modern ve işlevsel web siteleri tasarlayarak işletmelerin dijital dünyada güçlü bir varlık göstermelerini sağlıyoruz. Başarıya giden yolda profesyonel destek sunuyoruz.
-            </p>
-          </div>
-          <img src="https://picsum.photos/200/300" alt="" />
+        <Link
+          href="/hizmetlerimiz/web-site-cozumleri"
+          className="relative flex justify-center items-start px-12 flex-col group min-h-[50dvh]"
+        >
+          <p className="text-[#9c9ca9]">01</p>
+          <div className="h-[1px] w-5 bg-[#cfbe8c] mb-4 mt-0 group-hover:w-[50%] duration-500"></div>
+          <h2 className="text-[#cfbe8c] text-4xl">Web Site Çözümleri</h2>
+          <p className="text-[rgba(255,255,255,.7)] text-lg">
+            Millenium Digital, web sitesi çözümleri sunan bir firmadır.
+            Kullanıcı dostu, modern ve işlevsel web siteleri tasarlayarak
+            işletmelerin dijital dünyada güçlü bir varlık göstermelerini
+            sağlıyoruz. Başarıya giden yolda profesyonel destek sunuyoruz.
+          </p>
+          <img
+            src="https://picsum.photos/200/300"
+            alt=""
+            className="absolute top-0 left-0 w-full h-full object-cover z-[-1] filter brightness-[.3] group-hover:brightness-[.1] transition duration-300"
+          />
         </Link>
-        <Link href="/hizmetlerimiz/mobil-uygulama-cozumleri" className="card-area relative overflow-hidden">
-          <div className="text-area">
-            <p>02</p>
-            <h2>Mobil Uygulama Çözümleri</h2>
-            <p>
-            Millenium Digital, mobil uygulama çözümleri sunar. Yenilikçi ve kullanıcı dostu mobil uygulamalar tasarlayarak, işletmelerin dijital dünyada rekabet avantajı kazanmalarını sağlıyoruz. Mobil teknolojide başarıya ulaşmak için profesyonel destek sunuyoruz.
-            </p>
-          </div>
-          <img src="https://picsum.photos/200/300" alt="" />
+        <Link
+          href="/hizmetlerimiz/web-site-cozumleri"
+          className="relative flex justify-center items-start px-12 flex-col group min-h-[50dvh]"
+        >
+          <p className="text-[#9c9ca9]">02</p>
+          <div className="h-[1px] w-5 bg-[#cfbe8c] mb-4 mt-0 group-hover:w-[50%] duration-500"></div>
+          <h2 className="text-[#cfbe8c] text-4xl">Web Site Çözümleri</h2>
+          <p className="text-[rgba(255,255,255,.7)] text-lg">
+            Millenium Digital, web sitesi çözümleri sunan bir firmadır.
+            Kullanıcı dostu, modern ve işlevsel web siteleri tasarlayarak
+            işletmelerin dijital dünyada güçlü bir varlık göstermelerini
+            sağlıyoruz. Başarıya giden yolda profesyonel destek sunuyoruz.
+          </p>
+          <img
+            src="https://picsum.photos/200/300"
+            alt=""
+            className="absolute top-0 left-0 w-full h-full object-cover z-[-1] filter brightness-[.3] group-hover:brightness-[.1] transition duration-300"
+          />
         </Link>
-        <Link href="/hizmetlerimiz/profesyonel-marka-yonetimi" className="card-area relative overflow-hidden">
-          <div className="text-area">
-            <p>03</p>
-            <h2>Profesyonel Marka Yönetimi</h2>
-            <p>
-            Millenium Digital, profesyonel marka yönetimi hizmetleri sunar. Markanızı güçlendirmek ve hedef kitlenizle etkili iletişim kurmak için stratejik çözümler geliştiriyoruz. Markanızın değerini artırarak, sektörde fark yaratmanıza yardımcı oluyoruz.
-            </p>
-          </div>
-          <img src="https://picsum.photos/200/300" alt="" />
+        <Link
+          href="/hizmetlerimiz/web-site-cozumleri"
+          className="relative flex justify-center items-start px-12 flex-col group min-h-[50dvh]"
+        >
+          <p className="text-[#9c9ca9]">03</p>
+          <div className="h-[1px] w-5 bg-[#cfbe8c] mb-4 mt-0 group-hover:w-[50%] duration-500"></div>
+          <h2 className="text-[#cfbe8c] text-4xl">Web Site Çözümleri</h2>
+          <p className="text-[rgba(255,255,255,.7)] text-lg">
+            Millenium Digital, web sitesi çözümleri sunan bir firmadır.
+            Kullanıcı dostu, modern ve işlevsel web siteleri tasarlayarak
+            işletmelerin dijital dünyada güçlü bir varlık göstermelerini
+            sağlıyoruz. Başarıya giden yolda profesyonel destek sunuyoruz.
+          </p>
+          <img
+            src="https://picsum.photos/200/300"
+            alt=""
+            className="absolute top-0 left-0 w-full h-full object-cover z-[-1] filter brightness-[.3] group-hover:brightness-[.1] transition duration-300"
+          />
         </Link>
-        <Link href="/hizmetlerimiz/sosyal-medya-yonetimi" className="card-area relative overflow-hidden">
-          <div className="text-area">
-            <p>04</p>
-            <h2>Sosyal Medya Yönetimi</h2>
-            <p>
-            Millenium Digital, sosyal medya yönetimi hizmetleri sunar. Sosyal medya platformlarında markanızı etkin şekilde temsil eder, etkileşimi artırır ve hedef kitlenizle güçlü bir bağ kurmanızı sağlarız. Başarılı sosyal medya stratejileri ile fark yaratıyoruz.
-            </p>
-          </div>
-          <img src="https://picsum.photos/200/300" alt="" />
+        <Link
+          href="/hizmetlerimiz/web-site-cozumleri"
+          className="relative flex justify-center items-start px-12 flex-col group min-h-[50dvh]"
+        >
+          <p className="text-[#9c9ca9]">04</p>
+          <div className="h-[1px] w-5 bg-[#cfbe8c] mb-4 mt-0 group-hover:w-[50%] duration-500"></div>
+          <h2 className="text-[#cfbe8c] text-4xl">Web Site Çözümleri</h2>
+          <p className="text-[rgba(255,255,255,.7)] text-lg">
+            Millenium Digital, web sitesi çözümleri sunan bir firmadır.
+            Kullanıcı dostu, modern ve işlevsel web siteleri tasarlayarak
+            işletmelerin dijital dünyada güçlü bir varlık göstermelerini
+            sağlıyoruz. Başarıya giden yolda profesyonel destek sunuyoruz.
+          </p>
+          <img
+            src="https://picsum.photos/200/300"
+            alt=""
+            className="absolute top-0 left-0 w-full h-full object-cover z-[-1] filter brightness-[.3] group-hover:brightness-[.1] transition duration-300"
+          />
         </Link>
-        <Link href="/hizmetlerimiz/e-ticaret-cozumleri" className="card-area relative overflow-hidden">
-          <div className="text-area">
-            <p>05</p>
-            <h2>E-Ticaret Çözümleri</h2>
-            <p>
-            Millenium Digital, e-ticaret çözümleri sunar. İşletmenizi dijital pazarda başarılı kılmak için özelleştirilmiş e-ticaret platformları tasarlıyor ve geliştiriyoruz. Satışları artırmak ve müşteri memnuniyetini sağlamak için profesyonel destek sunuyoruz.
-            </p>
-          </div>
-          <img src="https://picsum.photos/200/300" alt="" />
+        <Link
+          href="/hizmetlerimiz/web-site-cozumleri"
+          className="relative flex justify-center items-start px-12 flex-col group min-h-[50dvh]"
+        >
+          <p className="text-[#9c9ca9]">05</p>
+          <div className="h-[1px] w-5 bg-[#cfbe8c] mb-4 mt-0 group-hover:w-[50%] duration-500"></div>
+          <h2 className="text-[#cfbe8c] text-4xl">Web Site Çözümleri</h2>
+          <p className="text-[rgba(255,255,255,.7)] text-lg">
+            Millenium Digital, web sitesi çözümleri sunan bir firmadır.
+            Kullanıcı dostu, modern ve işlevsel web siteleri tasarlayarak
+            işletmelerin dijital dünyada güçlü bir varlık göstermelerini
+            sağlıyoruz. Başarıya giden yolda profesyonel destek sunuyoruz.
+          </p>
+          <img
+            src="https://picsum.photos/200/300"
+            alt=""
+            className="absolute top-0 left-0 w-full h-full object-cover z-[-1] filter brightness-[.3] group-hover:brightness-[.1] transition duration-300"
+          />
         </Link>
-        <Link href="/hizmetlerimiz/crm-sistemleri" className="card-area relative overflow-hidden">
-          <div className="text-area">
-            <p>06</p>
-            <h2>CRM Çözümleri</h2>
-            <p>
-            Millenium Digital, CRM çözümleri sunar. Müşteri ilişkilerini yönetmek ve işletmenizin verimliliğini artırmak için özelleştirilmiş CRM sistemleri tasarlıyoruz. Müşteri memnuniyetini ve sadakatini artırmak için profesyonel destek sağlıyoruz.
-            </p>
-          </div>
-          <img src="https://picsum.photos/200/300" alt="" />
+        <Link
+          href="/hizmetlerimiz/web-site-cozumleri"
+          className="relative flex justify-center items-start px-12 flex-col group min-h-[50dvh]"
+        >
+          <p className="text-[#9c9ca9]">06</p>
+          <div className="h-[1px] w-5 bg-[#cfbe8c] mb-4 mt-0 group-hover:w-[50%] duration-500"></div>
+          <h2 className="text-[#cfbe8c] text-4xl">Web Site Çözümleri</h2>
+          <p className="text-[rgba(255,255,255,.7)] text-lg">
+            Millenium Digital, web sitesi çözümleri sunan bir firmadır.
+            Kullanıcı dostu, modern ve işlevsel web siteleri tasarlayarak
+            işletmelerin dijital dünyada güçlü bir varlık göstermelerini
+            sağlıyoruz. Başarıya giden yolda profesyonel destek sunuyoruz.
+          </p>
+          <img
+            src="https://picsum.photos/200/300"
+            alt=""
+            className="absolute top-0 left-0 w-full h-full object-cover z-[-1] filter brightness-[.3] group-hover:brightness-[.1] transition duration-300"
+          />
         </Link>
       </div>
     </div>
